c873f271d815a8d08ff558d358620e1dc86321ae
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-07 01:44:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-while True:
-    def display_even_numbers():
-        try:
-            start = int(input("Введіть початкове число: "))
-            end = int(input("Введіть кінцеве число: "))
+def display_square(side_length, symbol, filled):
+    if filled:
+        for _ in range(side_length):
+            print(symbol * side_length)
+    else:
+        print(symbol * side_length)
+        for _ in range(side_length - 2):
+            print(symbol + " " * (side_length - 2) + symbol)
+        print(symbol * side_length)
+side_length = int(input("Введіть довжину сторони квадрата: "))
+symbol = input("Введіть символ для відображення: ")
+filled = input("Заповнений квадрат (True) чи порожній (False)? (True/False): ").lower() == "true"
 
-            print(f"не парні числа між {start} і {end}:")
-            if start < end:
-                for num in range(start, end + 1):
-                    if num % 2 != 0:
-                        print(num)
-            else:
-                for num in range(end, start + 1):
-                    if num % 2 != 0:
-                        print(num)
-        except ValueError:
-            print("Будь ласка, введіть правильні числа.")
-
-
-    display_even_numbers()
-
-    продовжити = input('чи бажаєте ввести інші числа? (так/ні)')
-    if продовжити.lower() != 'так':
-        break
+display_square(side_length, symbol, filled)
