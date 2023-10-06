85477168abb68ba17f4d9b9b7298fc937a4114cb
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-07 01:58:17 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-while True:
-    def count_digits(number):
-        num_str = str(number)
-        digit_count = len(num_str)
-        return digit_count
-    num = int(input("Введіть число: "))
-    result = count_digits(num)
-    print(f"Кількість цифр у числі {num}: {result}")
-    продовжити = input("Чи бажаєте підоахувати кількість цифр в іншому числі?(так/ні):")
-    if продовжити.lower() !='так':
-        break
+def is_palindrome(number):
+    num_str = str(number)
+    return num_str == num_str[::-1]
+num = int(input("Введіть число: "))
+result = is_palindrome(num)
+if result:
+    print(f"{num} - це паліндром.")
+else:
+    print(f"{num} - не паліндром.")
