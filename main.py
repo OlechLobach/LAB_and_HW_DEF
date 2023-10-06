dae9f32ab96cb71367b5b7ea6945949232a888a6
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-07 01:31:26 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 def is_prime(number):
     if number <= 1:
         return False
-    if number <= 3:
-        return True
-    if number % 2 == 0 or number % 3 == 0:
-        return False
-    i = 5
-    while i * i <= number:
-        if number % i == 0 or number % (i + 2) == 0:
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
             return False
-        i += 6
+    return True
+number = int(input("Введіть число: "))
+result = is_prime(number)
 
-
-num = int(input('введіть число для перевірки'))
-result = is_prime(num)
 if result:
-    print(f"{num} є простим числом.")
+    print(f"{number} є простим числом.")
 else:
-    print(f"{num} не є простим числом.")
+    print(f"{number} не є простим числом.")
