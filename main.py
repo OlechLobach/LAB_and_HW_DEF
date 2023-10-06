31346e138124f80f1f3536bc28120238a49428e0
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-07 01:19:42 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-while True:
-    def display_even_numbers():
-        try:
-            start = int(input("Введіть початкове число: "))
-            end = int(input("Введіть кінцеве число: "))
+def display_line(length, direction, symbol):
+    if direction == "horizontal":
+        print(symbol * length)
+    elif direction == "vertical":
+        for _ in range(length):
+            print(symbol)
+    else:
+        print("Неправильний напрямок. Виберіть 'horizontal' або 'vertical'.")
 
-            print(f"Парні числа між {start} і {end}:")
-
-            # Перевірка, яке число є меншим і більшим
-            if start < end:
-                for num in range(start, end + 1):
-                    if num % 2 == 0:
-                        print(num)
-            else:
-                for num in range(end, start + 1):
-                    if num % 2 == 0:
-                        print(num)
-        except ValueError:
-            print("Будь ласка, введіть правильні числа.")
+length_h = int(input("Введіть горизонтальну довжину: "))
+symbol_h = input("Введіть символ для горизонтальної лінії: ")
+length_v = int(input("Введіть вертикальну довжину: "))
+symbol_v = input("Введіть символ для вертикальної лінії: ")
+direction_h = "horizontal"
+display_line(length_h, direction_h, symbol_h)
 
 
-    display_even_numbers()
-
-    продовжити = input('чи бажаєте ввести інші числа? (так/ні)')
-    if продовжити.lower() != 'так':
-        break
+direction_v = "vertical"
+display_line(length_v, direction_v, symbol_v)
